a06cd5060f98e5049110fa336e886afa3062ab3c
loremedda <98110186+loremedda@users.noreply.github.com>
2023-03-20 16:50:00 +0100

    PNPG-89 Modification of the pnpg dashboard with only the company name and tax code in the overview + replaced selfcare api url with the pnpg api path + clean code

---
+++ src/pages/dashboardOverview/components/activeProductsSection/components/DigitalNotificationCard.tsx
@@ -0,0 +1,70 @@
+import { Typography, Box, Grid, Card, useTheme, Tooltip, Fab } from '@mui/material';
+import { ProductAvatar } from '@pagopa/mui-italia';
+import ArrowForward from '@mui/icons-material/ArrowForward';
+
+type Props = {
+  cardTitle: React.ReactNode;
+  urlLogo: string;
+  btnAction?: () => void;
+};
+
+export default function DigitalNotificationCard({ cardTitle, urlLogo, btnAction }: Props) {
+  const theme = useTheme();
+
+  return (
+    <Card
+      raised
+      sx={{
+        borderRadius: theme.spacing(2),
+      }}
+    >
+      <Grid container p={2} pb={2}>
+        <Grid item xs={12} display="flex" alignItems="flex-start">
+          <Box display="flex" flexDirection="column" justifyContent="center" mr={2}>
+            <ProductAvatar
+              logoUrl={urlLogo}
+              size={'default'}
+              logoBgColor={'#0066CC'}
+              logoAltText={``}
+            />
+          </Box>
+          <Box display="flex" flexDirection="row" justifyContent="center">
+            {cardTitle && (
+              <Grid sx={{ width: 'fit-content' }} display="flex" alignItems={'center'} mt={1}>
+                <Tooltip title={cardTitle} placement="top" arrow={true}>
+                  <Typography
+                    variant="h6"
+                    sx={{
+                      fontSize: '24px',
+                      fontWeight: '600',
+                      height: '100%',
+                      width: '100%',
+                      overflow: 'hidden',
+                      textOverflow: 'ellipsis',
+                      display: '-webkit-box',
+                      WebkitBoxOrient: 'vertical' as const,
+                      WebkitLineClamp: 2,
+                      marginTop: 2,
+                    }}
+                  >
+                    {cardTitle}
+                  </Typography>
+                </Tooltip>
+              </Grid>
+            )}
+          </Box>
+          <Grid item sx={{ paddingTop: 4 }}>
+            <Fab
+              sx={{ alignSelf: 'end', marginLeft: 2 }}
+              size="small"
+              color="primary"
+              onClick={btnAction}
+            >
+              <ArrowForward />
+            </Fab>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Card>
+  );
+}
