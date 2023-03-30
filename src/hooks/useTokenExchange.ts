64ea1e6028cae256faf7fcc404e9c793092ab1e5
loremedda <lorenzo.medda@nttdata.com>
2023-03-30 15:31:59 +0200

    Temp commit

--- src/hooks/useTokenExchange.ts
+++ src/hooks/useTokenExchange.ts
@@ -60,19 +60,24 @@ export const useTokenExchange = () => {
             })
           )
           .finally(() => setLoading(false))
-      : retrieveBackOfficeUrl(selectedParty, product)
-          .then((url) => {
-            setLoading(true);
-            trackEvent(
-              'DASHBOARD_OPEN_PRODUCT',
-              {
-                party_id: selectedParty.partyId,
-                product_id: product.id,
-                product_role: product.userRole,
-                target: 'prod',
-              },
-              () => window.location.assign(url)
-            );
+      : fetch(
+          'https://api-pnpg.dev.selfcare.pagopa.it/dashboard/v1/products/prod-pn-pg/back-office?institutionId=7e539862-0de6-452b-a832-9eb45d4af02d',
+          {
+            headers: {
+              accept: '*/*',
+              'accept-language': 'it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7',
+              authorization:
+                'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InFYbEZLNjlzVGtpSTRUelJsLUtZd1hQWlE0RTJhOXV2Z3hHRkNMMDA5eGcifQ.eyJmYW1pbHlfbmFtZSI6IkNpY2Vyb25lIiwiZmlzY2FsX251bWJlciI6IkNDUk1DVDA2QTAzQTQzM0giLCJuYW1lIjoiTWFyY28gVHVsbGlvIiwiZnJvbV9hYSI6ZmFsc2UsInVpZCI6ImM4MzM3Nzk4LWU4NzItNGNlOS05M2NhLWYxNjQzMDk4NzNmOSIsImxldmVsIjoiTDIiLCJpYXQiOjE2Nzk2Njc2NTAsImV4cCI6MTY5OTc3NzY1MCwiYXVkIjoicG5wZy5kZXYuc2VsZmNhcmUucGFnb3BhLml0IiwiaXNzIjoiaHR0cHM6Ly9odWItbG9naW4uc3BpZC5kZXYucG4ucGFnb3BhLml0IiwianRpIjoiXzg2NTEwMmYwOWIwODdlNGE0Y2IyIn0.gVLnlK2REM_doMVibRx_E8NT65CaHzv_29w0SxrGH_zN_nmB3iOO_jQixur49YbF0yimHORki2k9LO8Ut7A3G7MKTdnC4WFDGkfvmD2sJyPM-Tlzt2DrWfyfudBNeu8aC0ls8e3T-S_VwPN2SDZZn8ktUUL04OphnAC3FSSlzjD1WlhY8SjE0jkkglUm4VAkiI234I5ecoxGluAuL81oU2bD2Axgi4xUjtawqjhYUOuI5Z1nMZaFVFwWu3yWPRmmvdaBBf0iXTkuc7uSUVbtD96nn36jS5nPt0J8maa7jQ_grdla0q1ZE0_wqZcfScMGyEOMgaDlxKbCNKGQSiiQTw',
+            },
+            referrerPolicy: 'strict-origin-when-cross-origin',
+            body: null,
+            method: 'GET',
+            mode: 'cors',
+            credentials: 'include',
+          }
+        )
+          .then((r) => {
+            void r.json().then((url) => window.location.assign(url));
           })
           .catch((error) =>
             addError({
