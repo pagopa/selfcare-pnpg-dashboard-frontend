15ce3de5d6657529b583a9f7ee7c140e14f61e2e
loremedda <lorenzo.medda@nttdata.com>
2023-04-03 11:00:02 +0200

    Removed forced writing of token to storage

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,8 @@
 import { Grid, Box, useTheme } from '@mui/material';
-import { useEffect, useMemo } from 'react';
+import { useMemo } from 'react';
 import { Route, Switch, useHistory } from 'react-router';
 import { useStore } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import { storageTokenOps } from '@pagopa/selfcare-common-frontend/utils/storage';
 import withSelectedParty from '../../decorators/withSelectedParty';
 import withProductRolesMap from '../../decorators/withProductsRolesMap';
 import withSelectedProduct from '../../decorators/withSelectedPartyProduct';
@@ -84,14 +83,6 @@ const Dashboard = () => {
   const store = useStore();
   const theme = useTheme();
   const { i18n } = useTranslation();
-
-  /* TODO At the moment, the token is manually written due to PN login service issues.
-   As soon as the outage is resolved, this TODO and implementation will be removed. */
-  useEffect(() => {
-    storageTokenOps.write(
-      'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InFYbEZLNjlzVGtpSTRUelJsLUtZd1hQWlE0RTJhOXV2Z3hHRkNMMDA5eGcifQ.eyJmYW1pbHlfbmFtZSI6IkNpY2Vyb25lIiwiZmlzY2FsX251bWJlciI6IkNDUk1DVDA2QTAzQTQzM0giLCJuYW1lIjoiTWFyY28gVHVsbGlvIiwiZnJvbV9hYSI6ZmFsc2UsInVpZCI6ImM4MzM3Nzk4LWU4NzItNGNlOS05M2NhLWYxNjQzMDk4NzNmOSIsImxldmVsIjoiTDIiLCJpYXQiOjE2Nzk2Njc2NTAsImV4cCI6MTY5OTc3NzY1MCwiYXVkIjoicG5wZy5kZXYuc2VsZmNhcmUucGFnb3BhLml0IiwiaXNzIjoiaHR0cHM6Ly9odWItbG9naW4uc3BpZC5kZXYucG4ucGFnb3BhLml0IiwianRpIjoiXzg2NTEwMmYwOWIwODdlNGE0Y2IyIn0.gVLnlK2REM_doMVibRx_E8NT65CaHzv_29w0SxrGH_zN_nmB3iOO_jQixur49YbF0yimHORki2k9LO8Ut7A3G7MKTdnC4WFDGkfvmD2sJyPM-Tlzt2DrWfyfudBNeu8aC0ls8e3T-S_VwPN2SDZZn8ktUUL04OphnAC3FSSlzjD1WlhY8SjE0jkkglUm4VAkiI234I5ecoxGluAuL81oU2bD2Axgi4xUjtawqjhYUOuI5Z1nMZaFVFwWu3yWPRmmvdaBBf0iXTkuc7uSUVbtD96nn36jS5nPt0J8maa7jQ_grdla0q1ZE0_wqZcfScMGyEOMgaDlxKbCNKGQSiiQTw'
-    );
-  }, []);
 
   const activeProducts: Array<Product> =
     useMemo(() => products?.filter((p) => p.productOnBoardingStatus === 'ACTIVE'), [products]) ??
