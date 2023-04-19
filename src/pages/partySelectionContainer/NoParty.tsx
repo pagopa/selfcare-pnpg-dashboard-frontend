6c0c618a6b1064117f22e18bc6e43905c43f5c29
loremedda <98110186+loremedda@users.noreply.github.com>
2023-04-19 11:43:24 +0200

    PNPG-122 New graphics and texts for noParty component

--- src/pages/partySelectionContainer/NoParty.tsx
+++ src/pages/partySelectionContainer/NoParty.tsx
@@ -1,67 +1,65 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Grid, Button, Typography, Box } from '@mui/material';
 import { trackEvent } from '@pagopa/selfcare-common-frontend/services/analyticsService';
-import { IllusError } from '@pagopa/mui-italia';
 import { useTranslation, Trans } from 'react-i18next';
+import { ReactComponent as NoBusiness } from '../../assets/no-business.svg';
 import { ENV } from '../../utils/env';
 
 export default function NoParty() {
   const { t } = useTranslation();
-
-  const bodyTitle = t('businessSelection.noBusinessFound.title');
 
   useEffect(() => {
     trackEvent('DASHBOARD_ASSOCIATION_FAILURE', { event_name: 'DASHBOARD_ASSOCIATION_FAILURE' });
   }, []);
 
   return (
-    <React.Fragment>
-      <Grid
-        direction="column"
-        container
-        display="flex"
-        justifyContent="center"
-        spacing={2}
-        my={'auto'}
-        sx={{ textAlign: 'center' }}
-      >
-        <Grid item container justifyContent="center">
-          <Grid item xs={4}>
-            <Grid mb={4}>
-              <IllusError size={70} />
-            </Grid>
-            <Box>
-              <Typography variant="h3" component="h2">
-                {bodyTitle}
-              </Typography>
-            </Box>
+    <Grid
+      direction="column"
+      container
+      display="flex"
+      justifyContent="center"
+      spacing={2}
+      my={'auto'}
+      sx={{ textAlign: 'center', display: 'flex', alignContent: 'center' }}
+    >
+      <Grid item container justifyContent="center">
+        <Grid item xs={4}>
+          <Grid mb={4}>
+            <NoBusiness />
           </Grid>
-        </Grid>
-        <Grid item container justifyContent="center">
-          <Grid item xs={4}>
-            <Box>
-              <Typography variant="body1">
-                <Trans i18nKey="businessSelection.noBusinessFound.description">
-                  Dal tuo SPID non risulti essere Legale Rappresentante di <br /> alcuna impresa.
-                </Trans>
-              </Typography>
-            </Box>
-          </Grid>
-        </Grid>
-
-        <Grid item xs={2} mt={3}>
-          <Button
-            variant="contained"
-            sx={{
-              height: '46px',
-              fontSize: 'fontSize',
-            }}
-            onClick={() => window.location.assign(ENV.URL_FE.ONBOARDING)}
-          >
-            {t('businessSelection.noBusinessFound.registerBusiness')}
-          </Button>
+          <Typography variant="h3" component="h2">
+            <Trans i18nKey="businessSelection.noBusinessFound.title">
+              L’impresa non è ancora <br />
+              registrata
+            </Trans>
+          </Typography>
         </Grid>
       </Grid>
-    </React.Fragment>
+      <Grid item container justifyContent="center">
+        <Grid item xs={4}>
+          <Box>
+            <Typography variant="body1">
+              <Trans i18nKey="businessSelection.noBusinessFound.description">
+                La registrazione può essere effettuata solo da un Legale <br />
+                Rappresentante.
+              </Trans>
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+
+      <Grid item xs={2} mt={3}>
+        <Button
+          variant="contained"
+          sx={{
+            height: '46px',
+            fontSize: 'fontSize',
+          }}
+          onClick={() => window.location.assign(ENV.URL_FE.ONBOARDING)}
+        >
+          {t('businessSelection.noBusinessFound.registerBusiness')}
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
