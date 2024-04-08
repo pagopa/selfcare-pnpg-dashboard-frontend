12ec53840495f889f0e047b510acfc2fadee4286
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2024-04-08 10:41:28 +0200

    SELC-5652 add env var to enable v2 in pnpg and switch api to v2

--- src/services/partyService.ts
+++ src/services/partyService.ts
@@ -5,6 +5,7 @@ import {
   institutionResource2Party,
   Party,
 } from '../model/Party';
+import { ENV } from '../utils/env';
 import { mockedBaseInstitutions, mockedInstitutions } from './__mocks__/partyService';
 
 export const fetchParties = (): Promise<Array<BaseParty>> => {
@@ -12,6 +13,11 @@ export const fetchParties = (): Promise<Array<BaseParty>> => {
   if (process.env.REACT_APP_MOCK_API === 'true') {
     return new Promise((resolve) => resolve(mockedBaseInstitutions));
   } else {
+    if (ENV.USER.ENABLE_USER_V2) {
+      return DashboardApi.getInstitutionsV2().then((institutions) =>
+        institutions ? institutions.map(institutionBaseResource2BaseParty) : []
+      );
+    }
     return DashboardApi.getInstitutions().then((institutions) =>
       institutions ? institutions.map(institutionBaseResource2BaseParty) : []
     );
@@ -24,6 +30,11 @@ export const fetchPartyDetails = (partyId: string): Promise<Party | null> => {
     const selectedPartyDetail = mockedInstitutions?.find((p) => p.partyId === partyId) ?? null;
     return new Promise((resolve) => resolve(selectedPartyDetail));
   } else {
+    if (ENV.USER.ENABLE_USER_V2) {
+      return DashboardApi.getInstitutionV2(partyId).then((institutionResource) =>
+        institutionResource ? institutionResource2Party(institutionResource) : null
+      );
+    }
     return DashboardApi.getInstitution(partyId).then((institutionResource) =>
       institutionResource ? institutionResource2Party(institutionResource) : null
     );
@@ -40,19 +51,6 @@ export const updateBusinessData = (
     return new Promise((resolve) => resolve(true));
   } else {
     return DashboardApi.updateBusinessData(institutionId, businessEmail, businessName);
-  }
-};
-
-export const retrieveProductBackoffice = (
-  productId: string,
-  institutionId: string,
-  environment?: string
-): Promise<string> => {
-  /* istanbul ignore if */
-  if (process.env.REACT_APP_MOCK_API === 'true') {
-    return new Promise((resolve) => resolve('dummyUrl'));
-  } else {
-    return DashboardApi.retrieveProductBackoffice(productId, institutionId, environment);
   }
 };
 
