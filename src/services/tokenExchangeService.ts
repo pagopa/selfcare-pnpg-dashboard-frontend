2e8441108e11e7dd7c741308c3b2133a51d6b40c
OraldoDoci <oraldo.doci@nttdata.com>
2024-06-05 15:55:06 +0200

    SELC-5011 upfate commons and remove v2 apis

--- src/services/tokenExchangeService.ts
+++ src/services/tokenExchangeService.ts
@@ -1,15 +1,9 @@
 import { DashboardApi } from '../api/DashboardApi';
 import { Party } from '../model/Party';
 import { Product } from '../model/Product';
-import { ENV } from '../utils/env';
 
 export const retrieveBackOfficeUrl = (
   selectedParty: Party,
   product: Product,
   environment?: string
-): Promise<string> => {
-  if (ENV.USER.ENABLE_USER_V2) {
-    return DashboardApi.retrieveProductBackofficeV2(product.id, selectedParty.partyId, environment);
-  }
-  return DashboardApi.retrieveProductBackoffice(product.id, selectedParty.partyId, environment);
-};
+): Promise<string> => DashboardApi.retrieveProductBackoffice(product.id, selectedParty.partyId, environment);
