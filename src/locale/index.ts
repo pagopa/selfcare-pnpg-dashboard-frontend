5a71a39949cc9e9e5e27241ac7c74790c5d25c64
OraldoDoci <oraldo.doci@nttdata.com>
2024-06-11 14:56:27 +0200

    SELC-5039 fix bug uplaod width check

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -3,6 +3,6 @@ import it from './it';
 import en from './en';
 import fr from './fr';
 import de from './de';
-import sl from './de';
+import sl from './sl';
 
 configureI18n({ i18n, it, en, fr, de, sl });
