b40607b49cd69ebc468685fa799d01c44977d9be
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2026-05-07 10:07:03 +0200

    temp fix for mixpanel

--- src/consentAndAnalyticsConfiguration.ts
+++ src/consentAndAnalyticsConfiguration.ts
@@ -1,5 +1,6 @@
 /* eslint-disable functional/immutable-data */
 import { CONFIG } from '@pagopa/selfcare-common-frontend/lib/config/env';
+import { initAnalytics } from '@pagopa/selfcare-common-frontend/lib/services/analyticsService';
 import { ENV } from './utils/env';
 
 CONFIG.ANALYTCS.ENABLE = ENV.ANALYTCS.ENABLE;
@@ -9,4 +10,27 @@ CONFIG.ANALYTCS.TOKEN = ENV.ANALYTCS.TOKEN;
 CONFIG.ANALYTCS.API_HOST = ENV.ANALYTCS.API_HOST;
 CONFIG.ANALYTCS.ADDITIONAL_PROPERTIES_IMPORTANT = { env: ENV.ENV };
 
-import '@pagopa/selfcare-common-frontend/lib/consentManagementConfigure';
+declare const OneTrust: any;
+declare const OnetrustActiveGroups: string;
+declare const window: any;
+
+// target cookies (Mixpanel)
+const targCookiesGroup = CONFIG.CONSENT.COOKIE_GROUP_ANALYTICS;
+
+// OneTrust callback at first time
+// eslint-disable-next-line functional/immutable-data
+window.OptanonWrapper = function () {
+  OneTrust.OnConsentChanged(function () {
+    const activeGroups = OnetrustActiveGroups;
+    if (activeGroups.indexOf(targCookiesGroup) > -1) {
+      initAnalytics();
+    }
+  });
+};
+// check mixpanel cookie consent in cookie
+const OTCookieValue: string =
+  document.cookie.split('; ').find((row) => row.startsWith('OptanonConsent=')) || '';
+const checkValue = `${targCookiesGroup}%3A1`;
+if (OTCookieValue.indexOf(checkValue) > -1) {
+  initAnalytics();
+}
